f608c225f8ee9908d43cd6ce5dbe036f18fda27d
Adam <channeladam@users.noreply.github.com>
2018-12-25 00:53:53 +1000

    Create chroot.sh

---
+++ VirtualBox/chroot.sh
@@ -0,0 +1,15 @@
+#############
+# Mount everything
+#############
+echo "Mounting"
+mount /dev/ArchVG/ArchRoot /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir /mnt/home
+mount /dev/ArchVG/ArchHome /mnt/home
+swapon /dev/ArchVG/ArchSwap
+
+#############
+# Chroot
+#############
+arch-chroot /mnt
