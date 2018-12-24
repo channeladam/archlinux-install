fc831555ff3f631c9250de542b4229b04dbb7b90
Adam <channeladam@users.noreply.github.com>
2018-12-24 23:33:02 +1000

    Initial commit

---
+++ VirtualBox/configure.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#############
+# Configure
+#############
+
+# fstab
+echo "fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Important: Manually check and edit fstab if necessary
+nano /mnt/etc/fstab
+
+# chroot
+echo "chroot"
+arch-chroot /mnt
+
+# timezone
+echo "timezone"
+ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+
+# generate /etc/adjtime, assuming UTC hardware clock
+echo "hardware clock"
+hwclock --systohc
+
+# localisation
+echo "localisation"
+locale-gen
+
+$locale="en_AU"
+# echo "LANG=en_AU.UTF-8" > /etc/locale.conf
+echo "LANG=$locale.UTF-8" > /etc/locale.conf
+echo "LC_COLLATE=C" >> /etc/locale.conf
+
+# comment out all other locales from locale.gen
+# sed -i '/'en_AU'.UTF-8/s/^#//g' /etc/locale.gen
+sed -i '/'$locale'.UTF-8/s/^#//g' /etc/locale.gen
+
+# hostname
+echo "hostname"
+$hostname="VLArch"
+# echo "VLArch" > /etc/hostname
+echo "$hostname" > /etc/hostname
+
+# hosts
+echo "hosts"
+echo "127.0.0.1    localhost" > /etc/hosts
+echo "::1          localhost" >> /etc/hosts
+echo "127.0.1.1    VLArch.localdomain    VLArch" >> /etc/hosts
+
+# Initramfs
+echo "initramfs"
+mkinitcpio -p linux
+
+# Root password
+echo "root password"
+passwd
+
+# Microcode (do this before bootloader)
+echo "microcode"
+pacman -S intel-ucode
+
+# Bootloader
+echo "bootloader"
+pacman -S grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "done"
+
+# umount -R /mnt
+# reboot
