014f20a4bf2bd976e1eb6864cc80a0072af93459
Adam <channeladam@users.noreply.github.com>
2018-12-25 00:55:28 +1000

    Update install.sh

--- VirtualBox/install.sh
+++ VirtualBox/install.sh
@@ -57,7 +57,7 @@ timedatectl set-ntp true
 echo "Mounting"
 mount /dev/ArchVG/ArchRoot /mnt
 mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/sda2 /mnt/boot
 mkdir /mnt/home
 mount /dev/ArchVG/ArchHome /mnt/home
 swapon /dev/ArchVG/ArchSwap
