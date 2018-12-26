473f3956145d9221af6b4affe420753d1c0d6852
Adam <channeladam@users.noreply.github.com>
2018-12-26 20:20:38 +1000

    Update install-pamac.sh

--- VirtualBox/install-pamac.sh
+++ VirtualBox/install-pamac.sh
@@ -2,9 +2,23 @@
 
 echo "NOTE: THIS CANNOT BE RUN AS ROOT"
 
-echo "pamac"
+echo "package manager - pamac"
 git clone https://aur.archlinux.org/pamac-aur.git ./pamac-tmp
 cd ./pamac-tmp
 makepkg -sic
 cd ..
 rm -rf ./pamac-tmp
+echo "*** Ensure you turn on AUR in the pamac preferences ***"
+
+echo "AUR package manager - pikaur"
+pamac build pikaur
+
+echo "system updater tray icon"
+#echo "pamac-tray-appindicator"
+#pikaur -S pamac-tray-appindicator
+echo "kalu"
+pikaur -S kalu
+
+echo "apps"
+pacman -S chromium firefox
+pacman -S code nodejs docker
