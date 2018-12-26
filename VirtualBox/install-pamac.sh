85d8264743f5ccbcc01960fcb73f1047c6417ff5
Adam <channeladam@users.noreply.github.com>
2018-12-26 22:03:57 +1000

    Update install-pamac.sh

--- VirtualBox/install-pamac.sh
+++ VirtualBox/install-pamac.sh
@@ -9,16 +9,3 @@ makepkg -sic
 cd ..
 rm -rf ./pamac-tmp
 echo "*** Ensure you turn on AUR in the pamac preferences ***"
-
-echo "AUR package manager - pikaur"
-pamac build pikaur
-
-echo "system updater tray icon"
-#echo "pamac-tray-appindicator"
-#pikaur -S pamac-tray-appindicator
-echo "kalu"
-pikaur -S kalu
-
-echo "apps"
-pacman -S chromium firefox
-pacman -S code nodejs docker
