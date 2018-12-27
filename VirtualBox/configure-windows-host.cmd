c3f4b7d847de82d211ae2ca6da5ec354524e503e
Adam <channeladam@users.noreply.github.com>
2018-12-27 15:56:45 +1000

    Create configure-windows-host.cmd

---
+++ VirtualBox/configure-windows-host.cmd
@@ -0,0 +1,6 @@
+# https://www.virtualbox.org/manual/ch10.html#customvesa
+"c:\program files\Oracle\VirtualBox\VBoxManage.exe" setextradata global GUI/MaxGuestResolution any
+"c:\program files\Oracle\VirtualBox\VBoxManage.exe" setextradata "Arch Linux" "CustomVideoMode1" "3440x1440x32"
+"c:\program files\Oracle\VirtualBox\VBoxManage.exe" setextradata "Arch Linux" "CustomVideoMode2" "3440x1200x32"
+"c:\program files\Oracle\VirtualBox\VBoxManage.exe" setextradata "Arch Linux" "CustomVideoMode3" "2560x1440x32"
+"c:\program files\Oracle\VirtualBox\VBoxManage.exe" setextradata "Arch Linux" "CustomVideoMode4" "2560x1200x32"
