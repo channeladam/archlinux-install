f1efac54fbd296837b52534243973ad18695f0e1
Adam <channeladam@users.noreply.github.com>
2020-03-14 21:59:44 +1000

    Added breeze icons

--- VirtualBox/install-xfce.sh
+++ VirtualBox/install-xfce.sh
@@ -15,25 +15,28 @@ echo "installing vmware video driver - assuming you are using the VirtualBox VMS
 #00:02.0 VGA compatible controller: VMware SVGA II Adapter
 pacman -S xf86-video-vmware
 
-echo "installing xfce"
+echo "installing xfce4"
 pacman -S xfce4 
 
-echo "installing some xfce goodies"
+echo "installing some of the xfce goodies"
 pacman -S thunar-archive-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpugraph-plugin xfce4-datetime-plugin xfce4-diskperf-plugin xfce4-mount-plugin xfce4-notifyd xfce4-screensaver xfce4-systemload-plugin xfce4-taskmanager xfce4-whiskermenu-plugin xfce4-artwork
 pacman -S xfce4-power-manager
 
 echo "install materia theme"
 pacman -S materia-gtk-theme
 
-echo "display manager"
+echo "install breeze theme and icons - primarily for the Breeze Dark icons"
+pacman -S breeze-gtk breeze-icons
+
+echo "install lightdm display manager"
 pacman -S lightdm lightdm-gtk-greeter
 systemctl enable lightdm
 
-echo "git"
+echo "install git"
 pacman -S git seahorse libsecret
 git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
 
-echo "sound"
+echo "install sound"
 # https://wiki.archlinux.org/index.php/Advanced_Linux_Sound_Architecture
 echo "options snd-intel8x0 ac97_clock=48000" >> /etc/modprobe.d/alsa-base.conf
 pacman -S alsa-utils alsa-oss alsa-firmware pulseaudio
