d6049a5bc3c7d998156310275cba3d25daca614d
Adam <channeladam@users.noreply.github.com>
2020-04-17 02:43:39 +1000

    Added script for creating encrypted partition

--- Laptop/partition-disk.sh
+++ Laptop/partition-disk.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 
 #############
-# Partition disks with LVM
-# Assuming a SSD disk of size 256GB 
+# Setup disk with encryption and LVM
 #############
 
-echo "Creating filesystems"
-
-
+echo "Creating GPT partition table"
 parted /dev/sda mktable gpt
 
-# /dev/sda1 = /boot (EFI System)
-# sgdisk --list-types
-# gdisk's internal code ef00 is for EFI System - boot, esp
-sgdisk /dev/sda -n=1:0:+512M -t=1:ef00
+echo "Creating partitions"
 
-# gdisk's internal code 8e00 is for Linux LVM
-sgdisk /dev/sda -N=2 -t2:8e00
+## /dev/sda1 = EFI System Partition (ESP)
+## sgdisk --list-types
+## gdisk's internal code ef00 is for EFI System - boot, esp
+sgdisk /dev/sda -n=1:0:+200M -t=1:ef00
 
+## /dev/sda2 = Bootloader (e.g. GRUB) mounted at /boot
+## gdisk internal code 8300 is for any linux file system
+sgdisk /dev/sda -n=2:201M:+312M -t=2:8300
+
+## /dev/sda3 = Encrypted Partition
+## gdisk's internal code 8309 is for Linux LUKS (encryption)
+sgdisk /dev/sda -N=3 -t3:8e00
 
 partprobe
-mkfs.vfat /dev/sda1
 
+echo "Performing random wipe on partition to be encrypted."
+echo "NOTE: a 256Gb disk will take ~20 minutes"
+dd bs=10M status=progress if=/dev/urandom of=/dev/sda3
 
-vgcreate ArchVG /dev/sda2
+echo "Creating file systems for ESP and Bootloader"
+mkfs.vfat /dev/sda1 -L esp
+mkfs.ext4 /dev/sda2 -L bootloader
+
+echo "Creating LUKS container for encrypted partition."
+echo "You will need to enter 'YES' and a passphrase."
+cryptsetup luksFormat /dev/sda3
+#Enter "YES"
+#Enter passphrase
+
+echo "Opening encrypted container."
+echo "You will need to enter the passphrase."
+# Becomes available at /dev/mapper/crypt-sda3
+cryptsetup luksOpen /dev/sda3 crypt-sda3
+
+echo "Creating the LVM group and volumes within the encrypted container."
+vgcreate ArchVG /dev/mapper/crypt-sda3
 
 #swapsize=$(cat /proc/meminfo | grep MemTotal | awk '{ print $2 }')
 #swapsize=$(($swapsize/1000))"M"
@@ -34,5 +55,11 @@ mkswap /dev/ArchVG/ArchSwap
 lvcreate -L 80G -n ArchRoot ArchVG
 mkfs.ext4 /dev/ArchVG/ArchRoot
 
-lvcreate -l 100%FREE -n ArchHome ArchVG
+#lvcreate -l 100%FREE -n ArchHome ArchVG
+lvcreate -L 100G -n ArchHome ArchVG
 mkfs.ext4 /dev/ArchVG/ArchHome
+
+echo "This is how it looks:"
+sgdisk -p /dev/sda
+lsblk
+lvdisplay
