9a6d49a8dc3de603a3642f56013530be2b99749a
Adam <adammcraven@gmail.com>
2021-01-10 00:05:06 +1000

    Exclude files in backup to nassy

--- laworkflo/backup-to-nassy.sh
+++ laworkflo/backup-to-nassy.sh
@@ -14,17 +14,52 @@ sudo chgrp -R adam /mnt/Backups/LUKS-headers
 sudo chgrp -R adam /mnt/Backups/Partitions
 
 # Backup
-echo "Backing up"
+echo "Backing up LUKS-headers"
 rsync -aPh /mnt/Backups/LUKS-headers/ $BACKUP_ROOT/LUKS-headers
+
+echo "Backing up Partitions"
 rsync -aPh /mnt/Backups/Partitions/ $BACKUP_ROOT/Partitions
 
-rsync -aPh /home/adam/Documents/ $BACKUP_ROOT/adam/Documents
-rsync -aPh /home/adam/scripts/ $BACKUP_ROOT/adam/scripts
-rsync -aPh /home/adam/repos/ $BACKUP_ROOT/adam/repos
-rsync -aPh /home/adam/.config/ $BACKUP_ROOT/adam/.config
-rsync -aPh /home/adam/.local/ $BACKUP_ROOT/adam/.local
+echo "Backing up .config"
+rsync -aPh \
+--exclude azuredatastudio/ \
+--exclude Code/ \
+--exclude Cache/ \
+--exclude cache/ \
+--exclude logs/ \
+/home/adam/.config/ $BACKUP_ROOT/adam/.config
+
+echo "Backing up .local"
+rsync -aPh \
+--exclude share/Trash \
+/home/adam/.local/ $BACKUP_ROOT/adam/.local
+
+echo "Backing up .thunderbird"
 rsync -aPh /home/adam/.thunderbird/ $BACKUP_ROOT/adam/.thunderbird
+
+echo "Backing up davmail.properties"
 rsync -aPh /home/adam/.davmail.properties $BACKUP_ROOT/adam/.davmail.properties
+
+echo "Backing up Documents"
+rsync -aPh \
+--exclude *.bak \
+/home/adam/Documents/ $BACKUP_ROOT/adam/Documents
+
+echo "Backing up scripts"
+rsync -aPh /home/adam/scripts/ $BACKUP_ROOT/adam/scripts
+
+echo "Backing up repos"
+rsync -aPh \
+--exclude .git/ \
+--exclude obj/ \
+--exclude bin/ \
+--exclude node_modules/ \
+--exclude **/*.log \
+--exclude **/*.bak \
+--exclude **/*.ldf \
+--exclude **/*.mdf \
+--exclude **/*.ndf \
+/home/adam/repos/ $BACKUP_ROOT/adam/repos 
 
 echo "Umounting"
 sudo umount /mnt/3copynassy
