bbb401f22ede29eb875822335db1c1f3b9f95eb5
Adam Craven <adammcraven@gmail.com>
2024-07-03 16:19:25 +1000

    Added Wine

--- laworkflo/install-apps.sh
+++ laworkflo/install-apps.sh
@@ -217,6 +217,10 @@ sudo systemctl enable --now libvirtd
 # Set the default virtual network to start automatically
 virsh net-autostart default
 
+# Wine
+# NOTE: Run winetricks -> winecfg -> Drives -> and add a T:\ pointing to /mnt/tdrive so that Wine apps can correctly see its free space!
+yay -S --noconfirm wine-staging winetricks
+
 
 #########################
 # Internet & Communications
